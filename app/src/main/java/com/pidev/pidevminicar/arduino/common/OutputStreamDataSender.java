091bfb32f9abfe1a78427b44908ee349b792f717
Maxim Piven <m.piven@youwe.nl>
2016-11-10 12:12:55 +0200

    added Input & Output Streams

---
+++ app/src/main/java/com/pidev/pidevminicar/arduino/common/OutputStreamDataSender.java
@@ -0,0 +1,32 @@
+package com.pidev.pidevminicar.arduino.common;
+
+import com.pidev.pidevminicar.arduino.OnDataReceivedListener;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by max on 10.11.16.
+ */
+public class OutputStreamDataSender extends Thread {
+
+    private OutputStream outputStream;
+    private boolean maintainConnection;
+    private OnDataReceivedListener listener;
+
+    public OutputStreamDataSender(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        maintainConnection = true;
+    }
+
+    public void close() {
+        this.maintainConnection = false;
+    }
+
+    @Override
+    public void run() {
+        while (maintainConnection) {
+            // todo: add sequence sending
+        }
+    }
+}
